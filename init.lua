66c7a1e2c2327ad7de0d5f80d7cee97ec6fb73b4
Kevin Tran <Kevin.Tran@amd.com>
2024-10-21 11:59:03 -0600

    4 size tab

--- init.lua
+++ init.lua
@@ -118,6 +118,12 @@ vim.opt.clipboard = 'unnamedplus'
 -- Enable break indent
 vim.opt.breakindent = true
 
+-- 4 size tab
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- Save undo history
 vim.opt.undofile = true
 
