0f3682732139a4b4f6584284bd7c727be7b78ba4
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-02-22 19:40:01 -0700

    make floating term opaque & slightly wider

--- lua/custom/float-term.lua
+++ lua/custom/float-term.lua
@@ -6,10 +6,10 @@ local state = {
   },
 }
 
--- Spawn basic window according to opts, or 70% height/width of current vim instance
+-- Spawn basic window according to opts, or scale based off current vim instance
 local function create_floating_window(opts)
   opts = opts or {}
-  local width = opts.width or math.floor(vim.o.columns * 0.7)
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
   local height = opts.height or math.floor(vim.o.lines * 0.7)
 
   local col = math.floor((vim.o.columns - width) / 2)
@@ -35,7 +35,7 @@ local function create_floating_window(opts)
   local win = vim.api.nvim_open_win(buf, true, win_config)
 
   -- Control degree of transparency
-  vim.wo[win].winblend = 20
+  vim.wo[win].winblend = 0
 
   return { buf = buf, win = win }
 end
